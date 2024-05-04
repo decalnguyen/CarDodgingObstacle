5ee40aa7bb750a9c87d915a9428ed2f7cc227a65
decalnguyen <nnntoan.it@gmail.com>
2024-05-04 11:16:44 +0700

    Add Servo function

--- doan.cpp
+++ doan.cpp
@@ -6,7 +6,27 @@ sbit IN3 = P2^2;
 sbit IN4 = P2^3;
 sbit BUTTON_RIGHT_PIN = P3^2;
 sbit BUTTON_LEFT_PIN = P3^7;
+sbit SERVO_PIN = P2^7;
 
+
+#define PWM_FREQUENCY 50   // 50Hz is a standard PWM frequency for servos
+#define TIMER_0_RELOAD (65536 - (F_CPU / (12 * PWM_FREQUENCY)))
+
+void timer0_setup() {
+    TMOD = 0x01;  // Timer 0, mode 1 (16-bit timer)
+    TH0 = TIMER_0_RELOAD >> 8;  // Set the initial reload value 
+    TL0 = TIMER_0_RELOAD & 0xFF; // Load timer registers
+    TR0 = 1;      // Start Timer 0
+    ET0 = 1;      // Enable Timer 0 interrupt (if you want to use interrupts)
+}
+
+void set_servo_angle(unsigned int angle) {
+    unsigned int pulse_width = (angle * 10) + 1000; // 1ms = 0 degrees, 2ms = 180 degrees (adjust if needed)
+    unsigned int reload_value = (65536 - pulse_width * (F_CPU / 12000000)); // Calculate timer reload value 
+
+    TH0 = reload_value >> 8; 
+    TL0 = reload_value & 0xFF; 
+}
 
 //Handle get distance 
 unsigned int get_distance()
@@ -37,7 +57,6 @@ unsigned int get_distance()
    
 }
 
-
 void delay_ms(unsigned int ms);
 
 //Handle go forward
@@ -50,9 +69,12 @@ void go_forward(int centimeters)
 
 //Handle go backward
 void go_backward(int centimeters)
-{
+{	
      IN3 = 0;
      IN4 = 1;
+     
+     IN3 = 1;
+     IN4 = 0;
 }
 
 //Handle turn left
@@ -62,6 +84,7 @@ void turn_left(int angle)
      if (steering_angle <-90) {	
 	 steering_angle = -90;
      }
+     set_servo_angel(angle);
 }
 
 //Handle turn right
@@ -71,6 +94,7 @@ void turn_right(int angle)
      if (steering_angle > 90) {
 	 steeing_angle = 90;
      }
+     set_servo_angel(angle);
 }
 
 //Handle button signal 
@@ -92,6 +116,7 @@ void init() {
 
 void main() {
    init();
+   P2.F0 = 0;
    unsigned int distance;
    int steering_angle = 0 ;
    //P1
